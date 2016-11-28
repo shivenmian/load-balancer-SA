458af9c494aa78e0e453e10f47b281dd52d617c2
Shiven Mian <shiven15094@iiitd.ac.in>
2016-11-28 11:44:24 +0530

    least conn

--- webapp/controllers/home.js
+++ webapp/controllers/home.js
@@ -2,8 +2,9 @@
  * GET /
  * Home page.
  */
-exports.index = (req, res) => {
-  res.render('home', {
-    title: 'Home'
-  });
-};
+
+ exports.index = (req, res) => {
+ 	res.render('home', {
+ 		title: 'Home'
+ 	});
+ };
---
+++ webapp/leastconn.js
@@ -0,0 +1,48 @@
+var httpProxy = require('http-proxy');
+var http = require('http');
+var seaport = require('seaport');
+
+var proxyservice = httpProxy.createProxyServer({});
+var args = process.argv.splice(2);
+var portlist = seaport.connect('localhost', 9090);
+var socketio = require('socket.io-client');
+var async = require('async');
+var _ = require('lodash');
+var params = {min: 1000, lcport: 12345};
+http.createServer(function (req, res) {
+	var x = {};
+	var serverlist = portlist.query('sa-webapp');
+	if (!serverlist.length) {
+		res.writeHead(503, {'Content-Type' : 'text/plain'});
+		res.end('No servers available to service your request');
+		return;
+	}
+	for(var p in serverlist){
+		var flag = 'port' + serverlist[p].port;
+		console.log(flag);
+		(function(flag) {
+			x[flag] = socketio.connect('http://localhost:' + serverlist[p].port, { 'force new connection':true });
+			console.log('connected ' + flag)
+		})(flag);
+	}
+	async.each(x, function(q, next) {
+		q.once('conncount', function(data){
+			var portkey = (_.findKey(x, q));
+			console.log(portkey.substring(4) + ' connected, count = ' + data.count);
+			if(data.count <= params.min){
+				params.min = data.count;
+				params.lcport = portkey.substring(4)
+			}
+			console.log('value of min is ' + params.min + ' and port is ' + params.lcport);
+		})
+		next();
+	})
+
+
+
+//    proxyservice.web(req, res, { target: ('http://localhost:'+ lcport)}); 
+
+
+//    i = (i + 1) % serverlist.length;
+//    proxyservice.web(req, res, { target: ('http://localhost:'+ serverlist[i].port)});   
+}).listen(args[0] || 8000);
